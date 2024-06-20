ef5be73d8de667d5f4becc70eb12ea45f733b554
Asaiba <144014192+Asaiba@users.noreply.github.com>
2024-06-21 00:18:03 +0200

    Up

--- logout.js
+++ logout.js
@@ -1,6 +1,6 @@
 document.getElementById('logout-btn').addEventListener('click', function () {
   
-    alert('You are logged out!');
+    alert('Logged out sucessfully!');
    
     window.location.href = 'index.html';
 });
